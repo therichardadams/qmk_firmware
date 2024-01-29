544c7d6e62bf51ffdd5cafb3ae6fc10382e1d390
Richard Adams <dev@radams.io>
2024-01-29 14:53:03 +0000

    Initial keymap commit

---
+++ keyboards/crkbd/rev1/keymaps/therichardadams/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
+#define MASTER_LEFT
+#define SPLIT_WPM_ENABLE
+#define OLED_BRIGHTNESS 143
+#define TAPPING_TERM 150
